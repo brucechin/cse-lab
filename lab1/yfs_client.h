3fed716c4c151fab8a425703876d0764724b08f1
Lianke <1026193951@sjtu.edu.cn>
2018-09-26 17:01:28 +0800

    .h change

--- lab1/yfs_client.h
+++ lab1/yfs_client.h
@@ -2,59 +2,73 @@
 #define yfs_client_h
 
 #include <string>
-//#include "yfs_protocol.h"
+
+// #include "yfs_protocol.h"
 #include "extent_client.h"
 #include <vector>
 
 
 class yfs_client {
-  extent_client *ec;
- public:
+    extent_client *ec;
 
-  typedef unsigned long long inum;
-  enum xxstatus { OK, RPCERR, NOENT, IOERR, EXIST };
-  typedef int status;
+public:
 
-  struct fileinfo {
-    unsigned long long size;
-    unsigned long atime;
-    unsigned long mtime;
-    unsigned long ctime;
-  };
-  struct dirinfo {
-    unsigned long atime;
-    unsigned long mtime;
-    unsigned long ctime;
-  };
-  struct dirent {
-    std::string name;
-    yfs_client::inum inum;
-  };
+    typedef unsigned long long inum;
+    enum xxstatus { OK, RPCERR, NOENT, IOERR, EXIST };
+    typedef int status;
 
- private:
-  static std::string filename(inum);
-  static inum n2i(std::string);
+    struct fileinfo {
+        unsigned long long size;
+        unsigned long      atime;
+        unsigned long      mtime;
+        unsigned long      ctime;
+    };
+    struct dirinfo {
+        unsigned long atime;
+        unsigned long mtime;
+        unsigned long ctime;
+    };
+    typedef fileinfo slinkinfo;
+    struct dirent {
+        std::string      name;
+        yfs_client::inum inum;
+    };
 
- public:
-  yfs_client();
-  yfs_client(std::string, std::string);
+private:
 
-  bool isfile(inum);
-  bool isdir(inum);
+    static std::string filename(inum);
+    static inum n2i(std::string);
 
-  int getfile(inum, fileinfo &);
-  int getdir(inum, dirinfo &);
+    int writedir(inum, std::list<dirent>&);
+    bool has_duplicate(inum, const char *);
+    bool add_entry_and_save(inum, const char *, inum);
 
-  int setattr(inum, size_t);
-  int lookup(inum, const char *, bool &, inum &);
-  int create(inum, const char *, mode_t, inum &);
-  int readdir(inum, std::list<dirent> &);
-  int write(inum, size_t, off_t, const char *, size_t &);
-  int read(inum, size_t, off_t, std::string &);
-  int unlink(inum,const char *);
-  int mkdir(inum , const char *, mode_t , inum &);
-  int writedir(inum dir, std::list<dirent>& entries);
-  /** you may need to add symbolic link related methods here.*/
+public:
+
+    yfs_client();
+    yfs_client(std::string,
+               std::string);
+
+    bool isfile(inum);
+    bool isdir(inum);
+
+    int getfile(inum, fileinfo&);
+    int getdir(inum, dirinfo&);
+    int getslink(inum, slinkinfo&);
+
+    int setattr(inum, size_t);
+    int create(inum, const char *, mode_t, inum&);
+    int read(inum, size_t, off_t, std::string&);
+    int write(inum, size_t, off_t, const char *, size_t&);
+    int unlink(inum, const char *);
+
+    int mkdir(inum, const char *, mode_t, inum&);
+    int readdir(inum, std::list<dirent>&);
+    int lookup(inum, const char *, bool&, inum&);
+
+    int symlink(inum, const char *, const char *, inum&);
+    int readslink(inum, std::string&);
+    /** you may need to add symbolic link related methods here.*/
 };
 
-#endif 
+#endif // ifndef yfs_client_h
