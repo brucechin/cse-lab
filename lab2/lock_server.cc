538e166d989bafa13da95548ab4f27bff61f89ff
Lianke <1026193951@sjtu.edu.cn>
2018-10-14 11:01:16 +0800

    1

--- lab2/lock_server.cc
+++ lab2/lock_server.cc
@@ -23,27 +23,48 @@ lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
 lock_protocol::status
 lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r)
 {
-  lock_protocol::status ret = lock_protocol::OK;
-	// Your lab2 part2 code goes here
-  pthread_mutex_lock(&map_mutex);
-  if (locks.find(lid) != locks.end()) {
-    while (locks[lid].granted == true) {
-        pthread_cond_wait(&locks[lid].wait, &map_mutex);
+  printf("[INFO] about to get mutex\n");
+    pthread_mutex_lock(&mutex);
+
+    // wait until lock is free
+    while (locks.find(lid) != locks.end()) {
+        pthread_cond_wait(&conds[lid], &mutex);
     }
-  }
-  locks[lid].granted = true;
-  pthread_mutex_unlock(&map_mutex);
-  return ret;
+
+    // now target lock is free and mutex is held
+    locks.insert(lid);  // acquire lock
+    printf("[INFO] client %d acquired lock %llu\n", clt, lid);
+
+    // craete condition variable if needed
+    if (conds.find(lid) == conds.end()) {
+        pthread_cond_t cond;
+        pthread_cond_init(&cond, NULL);
+        conds[lid] = cond;
+    }
+
+    pthread_mutex_unlock(&mutex);
+    printf("[INFO] just released mutex\n");
+    return lock_protocol::OK;
 }
 
 lock_protocol::status
 lock_server::release(int clt, lock_protocol::lockid_t lid, int &r)
 {
-  lock_protocol::status ret = lock_protocol::OK;
-	// Your lab2 part2 code goes here
-  pthread_mutex_lock(&map_mutex);
-  locks[lid].granted = false;
-  pthread_mutex_unlock(&map_mutex);
-  pthread_cond_signal(&locks[lid].wait);
+  printf("[INFO] about to get mutex\n");
+  pthread_mutex_lock(&mutex);
+
+  // if target lock is not existing
+  if (locks.find(lid) == locks.end()) {
+      printf("[ERROR] client %d tries to release un held lock %llu\n", clt, lid);
+      pthread_mutex_unlock(&mutex);
+      return lock_protocol::NOENT;
+  }
+
+  locks.erase(lid);  // release lock
+  printf("[INFO] client %d released lock %llu\n", clt, lid);
+  pthread_cond_signal(&conds[lid]);  // signal other threads that want this lock
+
+  pthread_mutex_unlock(&mutex);
+  printf("[INFO] just released mutex\n");
   return ret;
 }
