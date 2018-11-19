f26696e6b914501c6a0b265daba883b6b531020d
Lianke <1026193951@sjtu.edu.cn>
2018-11-19 17:48:15 +0800

    lab 3

--- lab3/lock_client_cache.cc
+++ lab3/lock_client_cache.cc
@@ -8,28 +8,20 @@
 #include <stdio.h>
 #include "tprintf.h"
 
-int lock_client_cache::last_port = 0;
 
 lock_client_cache::lock_client_cache(std::string xdst, 
 				     class lock_release_user *_lu)
   : lock_client(xdst), lu(_lu)
 {
-
-  srand(time(NULL)^last_port);
-  rlock_port = ((rand()%32000) | (0x1 << 10));
+  rpcs *rlsrpc = new rpcs(rlock_port);
+  rlsrpc->reg(rlock_protocol::revoke, this, &lock_client_cache::revoke_handler);
+  rlsrpc->reg(rlock_protocol::retry, this, &lock_client_cache::retry_handler);
 
   const char *hname;
   hname = "127.0.0.1";
   std::ostringstream host;
   host << hname << ":" << rlock_port;
-  last_port = rlock_port;
   id = host.str();
-
-  rpcs *rlsrpc = new rpcs(rlock_port);
-  rlsrpc->reg(rlock_protocol::revoke, this, &lock_client_cache::revoke_handler);
-  rlsrpc->reg(rlock_protocol::retry, this, &lock_client_cache::retry_handler);
-
-  
   VERIFY(pthread_mutex_init(&locks_mutex_, NULL) == 0);
   default_owner_ = pthread_self();
 }
@@ -125,6 +117,7 @@ bool lock_client_cache::lock_should_retry(lock_protocol::lockid_t lid){
 	locks_[lid].retry_ = false;
 	return ret;
 }
+
 
 
 void lock_client_cache::forget_lock(lock_protocol::lockid_t lid){
