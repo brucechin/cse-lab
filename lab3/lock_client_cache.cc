83bc66235e45303e423c04bd829a09ca703bdc6b
Lianke <1026193951@sjtu.edu.cn>
2018-11-19 18:24:40 +0800

    lab3

--- lab3/lock_client_cache.cc
+++ lab3/lock_client_cache.cc
@@ -8,20 +8,24 @@
 #include <stdio.h>
 #include "tprintf.h"
 
+int lock_client_cache::last_port = 0;
 
 lock_client_cache::lock_client_cache(std::string xdst, 
 				     class lock_release_user *_lu)
   : lock_client(xdst), lu(_lu)
 {
-  rpcs *rlsrpc = new rpcs(0);
-  rlsrpc->reg(rlock_protocol::revoke, this, &lock_client_cache::revoke_handler);
-  rlsrpc->reg(rlock_protocol::retry, this, &lock_client_cache::retry_handler);
-
+  srand(time(NULL)^last_port);
+  rlock_port = ((rand()%32000) | (0x1 << 10));
   const char *hname;
+  // VERIFY(gethostname(hname, 100) == 0);
   hname = "127.0.0.1";
   std::ostringstream host;
-  host << hname << ":" << rlsrpc->port();
+  host << hname << ":" << rlock_port;
   id = host.str();
+  last_port = rlock_port;
+  rpcs *rlsrpc = new rpcs(rlock_port);
+  rlsrpc->reg(rlock_protocol::revoke, this, &lock_client_cache::revoke_handler);
+  rlsrpc->reg(rlock_protocol::retry, this, &lock_client_cache::retry_handler);
   VERIFY(pthread_mutex_init(&locks_mutex_, NULL) == 0);
   default_owner_ = pthread_self();
 }
